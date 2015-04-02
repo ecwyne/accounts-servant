578223a37bba47ba00d647c5d03aa821a61b6c58
Eric Wyne <ecwyne@gmail.com>
2015-04-02 12:38:28 -0700

    change name

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 Package.describe({
   summary: 'Login service for Servant accounts',
-  version: '0.0.1'
+  version: '0.0.1',
+  name: 'ecwyne:accounts-servant'
 });
 
 Package.onUse(function(api) {
@@ -8,7 +9,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('servant', ['client', 'server']);
+  api.use('ecwyne:servant', ['client', 'server']);
 
   api.addFiles('servant_login_button.css', 'client');
 
