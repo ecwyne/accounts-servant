db17e846e1c11e71e6aa98add8d68a3276148b7e
Eric Wyne <ecwyne@gmail.com>
2015-04-02 22:21:29 -0700

    export sdks

--- servant.js
+++ servant.js
@@ -17,6 +17,6 @@ if (Meteor.isClient) {
     // thus not sure if we should be sending access tokens; but we do it
     // for all other oauth2 providers, and it may come in handy.
     forLoggedInUser: ['services.servant'],
-    forOtherUsers: ['services.servant.username']
+    forOtherUsers: ['']
   });
 }
